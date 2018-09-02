08754afb34cad6a78c560e69226bbdad32cacb71
Eduardo Castillo <eduardocastillo@Eduardos-MacBook-Pro.local>
2018-09-02 18:03:08 -0500

    Se actualizan operaciones

--- src/main/java/com/example/demo/SpringBootJpaRepositoryApplication.java
+++ src/main/java/com/example/demo/SpringBootJpaRepositoryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.jpa.entity.Person;
 import com.example.demo.jpa.repository.PersonJpaRepository;
 
 @SpringBootApplication
@@ -25,5 +26,15 @@ public class SpringBootJpaRepositoryApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		logger.info("User id 101 -> {} ", repository.findById(101));
 		
+		logger.info("Insert 108 -> {} ", repository.insert(new Person(108, "Estrella Arturo", "Infinity")));
+		
+		logger.info("Update 103 -> {} ", repository.update(new Person(103, "Eduardo Update", "Jupiter Update")));
+		
+		logger.info("All users before delete -> {}", repository.findAll());
+		
+		 repository.deleteById(102);
+		 
+		 logger.info("All user after delete -> {}", repository.findAll());
+		 
 	}
 }
